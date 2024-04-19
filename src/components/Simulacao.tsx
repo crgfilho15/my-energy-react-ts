3b15fd434eb78ee63179e92ec39678c69a19b24f
Aluno <aluno@senai.br>
2024-04-18 21:09:09 -0300

    Atualizando

---
+++ src/components/Simulacao.tsx
@@ -0,0 +1,9 @@
+import styles from './Simulacao.module.css';
+
+export function Simulacao() {
+    return(
+        <div className={styles.simulacao}>
+            <h1>SIMULAÇÃO - EM BREVE</h1>
+        </div>
+    );
+}
